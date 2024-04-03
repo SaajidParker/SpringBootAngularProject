f8e306924c3bd310b35bed56c7470dc101260d87
Saajid Parker <parkersaajid2@gmail.com>
2024-04-03 13:56:10 +0200

    "Editing Employee Class"

--- src/main/java/tech/getarrays/employeemanager/model/Employee.java
+++ src/main/java/tech/getarrays/employeemanager/model/Employee.java
@@ -1,8 +1,16 @@
 package tech.getarrays.employeemanager.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.io.Serializable;
 
+@Entity
 public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
